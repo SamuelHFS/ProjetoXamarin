cf52af5a89031bd4401c5ae82945d0fe6fb1e5e4
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-16 11:20:08 -0300

    teste1

---
+++ ProjetoXama/ProjetoXama/ProjetoXama/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjetoXama
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btEnviar(object sender, EventArgs e)
+        {
+            DisplayAlert("Aviso", "A vida é bela! Viva a vida planamente", "Fechar");
+        }
+    }
+}
---
+++ ProjetoXama/ProjetoXama/ProjetoXama/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetoXama
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            StackLayout stk = new StackLayout()
+            { 
+                Orientation = StackOrientation.Vertical
+            };
+            Label lb1 = new Label()
+            {
+                Text = "curso Xamarin",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 24,
+                BackgroundColor = Color.Black,
+                Padding = new Thickness(10),
+                TextColor = Color.White,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+            stk.Children.Add(lb1);
+
+            Content = stk;
+
+        }
+    }
+}
