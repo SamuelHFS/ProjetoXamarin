94c6cd029b8a38bc6acefb1861a552bc71698efb
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-17 15:37:12 -0300

    nada alterado

--- ProjetoXama/ProjetoXama/ProjetoXama/Page1.xaml.cs
+++ ProjetoXama/ProjetoXama/ProjetoXama/Page1.xaml.cs
@@ -22,7 +22,7 @@ namespace ProjetoXama
             };
             Label lb1 = new Label()
             {
-                Text = "curso ",
+                Text = "curso taç",
                 FontAttributes = FontAttributes.Bold,
                 FontSize = 24,
                 BackgroundColor = Color.Black,
